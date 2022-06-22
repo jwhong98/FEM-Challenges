246a3cf204fdbdabf578c5bdb73da5f63e4adad6
jwhong <jwhong98@gmail.com>
2022-06-22 10:11:18 -0700

    base component layout

---
+++ rock-paper-scissors/src/components/MovesButton/moves.js
@@ -0,0 +1,17 @@
+export const rock = {
+  id: "rock",
+  img: require("../../images/icon-rock.svg").default,
+  color: "linear-gradient(hsl(349, 71%, 52%), hsl(349, 70%, 56%))",
+};
+
+export const paper = {
+  id: "paper",
+  img: require("../../images/icon-paper.svg").default,
+  color: "linear-gradient(hsl(230, 89%, 62%), hsl(230, 89%, 65%))",
+};
+
+export const scissors = {
+  id: "scissors",
+  img: require("../../images/icon-scissors.svg").default,
+  color: "linear-gradient(hsl(39, 89%, 49%), hsl(40, 84%, 53%))",
+};
