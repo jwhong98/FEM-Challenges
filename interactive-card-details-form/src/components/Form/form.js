1314162845e226d1697499fe6bc4ac4989f8207f
jwhong <jwhong98@gmail.com>
2022-08-23 13:06:08 -0700

    added cards in header section

--- interactive-card-details-form/src/components/Form/form.js
+++ interactive-card-details-form/src/components/Form/form.js
@@ -2,11 +2,19 @@ import React from "react";
 import classes from "./Form.module.css";
 
 const Form = () => {
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <form className={classes.form}>
       <div className={classes.info}>
         <label htmlFor="name">cardholder name</label>
-        <input type="text" id="name" placeholder="e.g. Jane Appleseed" />
+        <input
+          type="text"
+          id="name"
+          placeholder="e.g. Jane Appleseed"
+          required
+        />
       </div>
       <div className={classes.info}>
         <label htmlFor="cardNumber">card number</label>
@@ -14,6 +22,7 @@ const Form = () => {
           type="text"
           id="cardNumber"
           placeholder="e.g. 1234 5678 9123 0000"
+          required
         />
       </div>
 
@@ -21,12 +30,16 @@ const Form = () => {
         <label htmlFor="date">exp. date (mm/yy)</label>
         <label htmlFor="cvc">cvc</label>
         <div className={classes.dateInput}>
-          <input type="text" id="date" placeholder="MM" />
-          <input type="text" id="date" placeholder="YY" />
+          <input type="text" id="date" placeholder="MM" required />
+          <input type="text" id="date" placeholder="YY" required />
         </div>
-        <input type="text" id="cvc" placeholder="e.g. 123" />
+        <input type="text" id="cvc" placeholder="e.g. 123" required />
       </div>
-      <button type="submit" className={classes.submit}>
+      <button
+        type="submit"
+        className={classes.submit}
+        onClick={onSubmitHandler}
+      >
         Confirm
       </button>
     </form>
--- interactive-card-details-form/src/components/Main/main.js
+++ interactive-card-details-form/src/components/Main/main.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Form from "../Form/Form";
-import cardBack from "../../images/bg-card-back.png";
-import cardFrom from "../../images/bg-card-front.png";
+import logo from "../../images/card-logo.svg";
 import classes from "./Main.module.css";
 
 const Main = () => {
   return (
     <main>
       <header>
-        {/* <img src={cardBack} alt="" className="headerImg" /> */}
+        <div className={classes.cardBack}>
+          <p className={classes.cvc}>000</p>
+        </div>
+        <div className={classes.cardFront}>
+          <img className={classes.cardLogo} src={logo} alt="" />
+          <div className={classes.cardFrontInfo}>
+            <p className={classes.cardNum}>0000 0000 0000 0000</p>
+            <div className={classes.sub}>
+              <p className={classes.cardName}>jane appleseed</p>
+              <p className={classes.cardExp}>00/00</p>
+            </div>
+          </div>
+        </div>
       </header>
       <Form />
     </main>
